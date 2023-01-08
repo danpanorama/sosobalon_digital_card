2e83a8ed3a7b7a719fa5086e328a15940837289d
danpanorama <62213659+danpanorama@users.noreply.github.com>
2023-01-08 15:09:43 +0200

    change image

--- js/js.js
+++ js/js.js
@@ -32,7 +32,7 @@ window.addEventListener('load',(e)=>{
             console.log(userEmail, userName, text);
             var num = '972549326864'
             
-  var win = window.open(`https://wa.me/972534273529?text=שלום%27לך%סוסו%20קוראים%20לי%20${userName}%20%20${text}`, '_blank')
+  var win = window.open(`https://wa.me/+972546494586?text=שלום%27לך%סוסו%20קוראים%20לי%20${userName}%20%20${text}`, '_blank')
          
        win()
 }
